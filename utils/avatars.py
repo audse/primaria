0ee0eefa763b06163c8ee387f83c9c84540a6920
Audrey Serene <audrey.serene@gmail.com>
2023-12-22 13:49:29 -0600

    Format utils app

--- utils/avatars.py
+++ utils/avatars.py
@@ -1,18 +1,20 @@
-
 import random
 from datetime import datetime
 from core.models import Pet, Avatar
 from social.models import Message
 
-def send_avatar_message (request, avatar_name):
+
+def send_avatar_message(request, avatar_name):
     Message.objects.create(
-            receiving_user=request.user,
-            subject="You just found a secret avatar!",
-            text="You have just received the avatar \"%s\" to use on the boards!"%(avatar_name)
-        )
+        receiving_user=request.user,
+        subject="You just found a secret avatar!",
+        text='You have just received the avatar "%s" to use on the boards!'
+        % (avatar_name),
+    )
+
 
 # Get the "Feeling Blue" avatar when you have a blue pet and 5 or more blue items in your inventory
-def get_feeling_blue (request, inventory_items):
+def get_feeling_blue(request, inventory_items):
     feeling_blue_avatar = Avatar.objects.get(url="feeling-blue")
     if feeling_blue_avatar not in request.user.profile.avatars.all():
         pet = Pet.objects.filter(user=request.user).first()
@@ -24,19 +26,21 @@ def get_feeling_blue (request, inventory_items):
             if blue_items >= 5:
                 request.user.profile.avatars.add(feeling_blue_avatar)
                 request.user.profile.save()
-                send_avatar_message(request, 'Feeling Blue')
+                send_avatar_message(request, "Feeling Blue")
+
 
 # Get the "Sweet~" avatar when you have a sprinkle pet and you feed it a cupcake
-def get_sweet (request, pet, inventory_item):
+def get_sweet(request, pet, inventory_item):
     sweet_avatar = Avatar.objects.get(url="sweet")
     if sweet_avatar not in request.user.profile.avatars.all():
         if pet.color == "sprinkle" and "cupcake" in inventory_item.item.name.lower():
             request.user.profile.avatars.add(sweet_avatar)
             request.user.profile.save()
-            send_avatar_message(request, 'Sweet~')
+            send_avatar_message(request, "Sweet~")
+
 
 # Get the "Starry Night" avatar when you use a starry potion at night
-def get_starry_night (request, pet):
+def get_starry_night(request, pet):
     starry_night_avatar = Avatar.objects.get(url="starry-night")
     if starry_night_avatar not in request.user.profile.avatars.all():
         if pet.color == "starry":
@@ -44,10 +48,11 @@ def get_starry_night (request, pet):
             if today.hour > 18 and today.hour < 24 or today.hour > 0 and today.hour < 6:
                 request.user.profile.avatars.add(starry_night_avatar)
                 request.user.profile.save()
-                send_avatar_message(request, 'Starry Night')
+                send_avatar_message(request, "Starry Night")
+
 
 # Get the "Witch!!!" avatar at random when you use a morph potion
-def get_witch (request):
+def get_witch(request):
     rand_num = random.randint(1, 4)
     if rand_num == 1:
         witch_avatar = Avatar.objects.get(url="witch")
@@ -56,18 +61,20 @@ def get_witch (request):
             request.user.profile.save()
             send_avatar_message(request, "Witch!!!")
 
+
 # Get the "That Tasted Funny..." avatar at random when feeding your pet a hunger stat potion
-def get_that_tasted_funny (request):
+def get_that_tasted_funny(request):
     rand_num = random.randint(1, 4)
     if rand_num == 1:
         funny_avatar = Avatar.objects.get(url="that-tasted-funny")
         if funny_avatar not in request.user.profile.avatars.all():
             request.user.profile.avatars.add(funny_avatar)
             request.user.profile.save()
-            send_avatar_message(request, 'That Tasted Funny...')
+            send_avatar_message(request, "That Tasted Funny...")
+
 
 # Get the "Yay! Pink!!" avatar when purchasing a pink item
-def get_yay_pink (request, inventory_item):
+def get_yay_pink(request, inventory_item):
     pet = Pet.objects.filter(user=request.user).first()
     if pet is not None:
         pink_avatar = Avatar.objects.get(url="yay-pink")
@@ -75,4 +82,4 @@ def get_yay_pink (request, inventory_item):
             if "pink" in inventory_item.item.name.lower():
                 request.user.profile.avatars.add(pink_avatar)
                 request.user.profile.save()
-                send_avatar_message(request, 'Yay! Pink!!')
+                send_avatar_message(request, "Yay! Pink!!")
--- utils/error.py
+++ utils/error.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
 
+
 def error_page(request):
-    error = request.session.pop('error', False)
+    error = request.session.pop("error", False)
     if error == False:
         error = "We are not sure what, but we will work on fixing it."
-    return render(request, 'core/error_page.html', {'error':error})
+    return render(request, "core/error_page.html", {"error": error})
+
 
 def handle_error(request, message):
-    request.session['error'] = message
-    return render(request, 'core/error_page.html', {'error':message})
+    request.session["error"] = message
+    return render(request, "core/error_page.html", {"error": message})
+
 
 def require_login(request):
     if not request.user.is_authenticated():
-        return render(request, 'core/error_page.html', {'error':"You must be logged in to view this page."})
+        return render(
+            request,
+            "core/error_page.html",
+            {"error": "You must be logged in to view this page."},
+        )
